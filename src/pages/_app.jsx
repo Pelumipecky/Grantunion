8a1eb41e300f10def1a86fa03d5cf93a7aa79b62
DEv rox <midlandcakess@gmail.com>
2025-12-09 08:41:17 +0100

    UI: Use grantunionsmall.png as favicon and apple-touch-icon

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -86,8 +86,8 @@ export default function App({ Component, pageProps }) {
         <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
         <meta name="theme-color" content='#1C0F36'/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <link rel="icon" href="/grant-union-icon.png"/>
-        <link rel="apple-touch-icon" href="/grant-union-icon.png"/>
+        <link rel="icon" href="/grantunionsmall.png"/>
+        <link rel="apple-touch-icon" href="/grantunionsmall.png"/>
         <meta property="og:title" content="Grant Union"/>
         <meta property="og:description" content="Grant Union is a trusted binary and cryptocurrency trading company delivering consistent, compliant returns."/>
       </Head>
